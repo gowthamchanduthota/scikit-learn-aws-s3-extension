de044b063ceeeecc1ad1cb31c647b69792cffe7d
mohith kumar <kilarimohithkumar@gmail.com>
2024-11-12 03:00:19 +0530

    Update DataGenerator.py
    Added logging for batch processing and a simple data augmentation step to the __data_generation method.

--- sprint-2/DataGenerator.py
+++ sprint-2/DataGenerator.py
@@ -4,6 +4,11 @@ import boto3
 from PIL import Image
 import io
 from s3helper import *
+import logging
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class S3DataGenerator(tf.keras.utils.Sequence):
     def __init__(self, bucket_name, file_keys, labels_age, labels_gender, batch_size, img_size=(128, 128), n_channels=1, shuffle=True, aws_access_key_id=None, aws_secret_access_key=None):
@@ -16,10 +21,6 @@ class S3DataGenerator(tf.keras.utils.Sequence):
         self.n_channels = n_channels
         self.shuffle = shuffle
         self.s3_client = S3Helper(datasets_bucket=bucket_name)
-        # self.s3_client = S3Helper(datasets_bucket=bucket_name, credentials={
-        #     "AWS_ACCESS_KEY_ID" :  aws_access_key_id,
-        #     "AWS_SECRET_ACCESS_KEY": aws_secret_access_key
-        # })
         self.indexes = np.arange(len(self.file_keys))
         self.on_epoch_end()
 
@@ -41,16 +42,17 @@ class S3DataGenerator(tf.keras.utils.Sequence):
         X = np.empty((self.batch_size, *self.img_size, self.n_channels))
         y_age = np.empty((self.batch_size), dtype=float)
         y_gender = np.empty((self.batch_size), dtype=int)
-        print(X.shape)
+        logger.info(f"Processing batch of {len(batch_file_keys)} images.")  # Added logging for batch processing
         for i, file_key in enumerate(batch_file_keys):
-            # response = self.s3_client.get_object(Bucket=self.bucket_name, Key="UTKFace/" + file_key)
-            # image_data = response['Body'].read()
             image_data = self.s3_client.get_img("UTKFace/" + file_key)
-            # image = Image.open(io.BytesIO(image_data)).resize(self.img_size).convert('L')
             image = image_data.resize(self.img_size).convert('L')
             image = np.array(image)
+            
+            # Simple augmentation: Horizontal flip with 50% chance
+            if np.random.rand() > 0.5:
+                image = np.fliplr(image)
+            
             X[i,] = np.expand_dims(image, axis=-1) / 255.0  # Normalize
-
             y_age[i] = self.labels_age[batch_indexes[i]]
             y_gender[i] = self.labels_gender[batch_indexes[i]]
 
