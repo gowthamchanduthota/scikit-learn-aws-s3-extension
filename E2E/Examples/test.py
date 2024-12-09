2773c6ce0dc777503d5559c69d25ac4168beeb3e
gowthamchanduthota <gowthamchanduthota@outlook.com>
2024-12-08 20:34:49 -0600

    Added unit test cases

--- E2E/Examples/test.py
+++ E2E/Examples/test.py
@@ -1,3 +1,14 @@
-from ..library import DataGenerator, ModelWrapper, s3helper
+import base64
 
+def encode(data):
+    encoded_bytes = base64.b64encode(data.encode('utf-8'))
+    encoded_str = encoded_bytes.decode('utf-8')
+    return encoded_str
 
+def decode(data):
+    decoded_bytes = base64.b64decode(data)
+    decoded_string = decoded_bytes.decode('utf-8')
+    return decoded_string
+
+print(encode("AKIA4MI2JSNSQVVGU7QP"))
+print(encode("DABRHb8j51TyceoSOWsEAnkNedJHMatgy6O/IUsm"))
